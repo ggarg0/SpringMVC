2a53685be641c598d0768e9af5214681c7026b53
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-02-27 18:28:41 +0530

    Commit
    Commit

--- SpringAnnotations/src/com/gaurav/annotations/MainAppAnnotations.java
+++ SpringAnnotations/src/com/gaurav/annotations/MainAppAnnotations.java
@@ -4,25 +4,20 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
 public class MainAppAnnotations {
 
 	public static void main(String[] args) {
-		  ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 
-		    /*  HelloWorld objA = (HelloWorld) context.getBean("helloWorld");
+		Shape obj = (Shape) context.getBean("triangle");
+		obj.draw();
 
-		      objA.getMessage1();
-		      objA.getMessage2();*/
-		  	((AbstractApplicationContext) context).registerShutdownHook();
-		      Shape obj = (Shape) context.getBean("triangle");
-		      obj.draw();
-		      
-		      // @Required annotation and org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor
-		      obj = (Shape)  context.getBean("circle");
-		      obj.draw();
-		      System.out.println("MessageSource message : " + 
-		      context.getMessage("greeting",null,"Default message",null));
+		// @Required annotation and
+		// org.springframework.beans.factory.annotation.RequiredAnnotationBeanPostProcessor
+		obj = (Shape) context.getBean("circle");
+		obj.draw();
+		System.out.println("MessageSource message : " + context.getMessage("greeting", null, "Default message", null));
+		((AbstractApplicationContext) context).registerShutdownHook();
 	}
 
 }
