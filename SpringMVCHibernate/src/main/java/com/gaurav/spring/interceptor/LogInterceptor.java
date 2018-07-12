4b61ccc56db31c06ffb89b198526c4f5d22d73ae
Gaurav Garg <30888086+ggarg0@users.noreply.github.com>
2018-07-12 18:52:04 +0530

    Commit

---
+++ SpringMVCHibernate/src/main/java/com/gaurav/spring/interceptor/LogInterceptor.java
@@ -0,0 +1,30 @@
+package com.gaurav.spring.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LogInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		System.out.println(" After Completion ");
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		System.out.println(" postHandle ");
+
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2) throws Exception {
+		System.out.println(" preHandle ");
+		return true;
+	}
+
+}
